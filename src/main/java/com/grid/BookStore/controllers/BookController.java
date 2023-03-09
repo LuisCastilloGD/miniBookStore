ad34b73b127a25c536e497ef88326c9ae88d9289
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-09 16:50:18 -0600

    change allargsconstructor to requiredargsconstructor

--- src/main/java/com/grid/BookStore/controllers/BookController.java
+++ src/main/java/com/grid/BookStore/controllers/BookController.java
@@ -4,9 +4,7 @@ import com.grid.BookStore.models.dtos.BookDto;
 import com.grid.BookStore.models.Book;
 import com.grid.BookStore.services.BookService;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,32 +14,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/mini_store")
 public class BookController {
 
-    private BookService bookService;
+    private final BookService bookService;
 
     @PostMapping("/add")
-    public ResponseEntity<BookDto> addBook(@Valid @RequestBody Book book){
-        return new ResponseEntity<>(bookService.addBook(book), HttpStatus.OK);
+    public BookDto addBook(@Valid @RequestBody Book book){
+        return bookService.addBook(book);
     }
 
     @GetMapping("/get/{id}")
-    public ResponseEntity<BookDto> getBook(@PathVariable("id") Long id){
-        return new ResponseEntity<>(bookService.getBook(id), HttpStatus.OK);
+    public BookDto getBook(@PathVariable("id") Long id){
+        return bookService.getBook(id);
     }
 
     @PutMapping("/edit/{id}")
-    public ResponseEntity<BookDto> editBook(@PathVariable("id") Long id, @Valid @RequestBody Book book){
-        return new ResponseEntity<>(bookService.editBook(id,book), HttpStatus.OK);
+    public BookDto editBook(@PathVariable("id") Long id, @Valid @RequestBody Book book){
+        return bookService.editBook(id,book);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<BookDto> deleteBook(@PathVariable("id") Long id){
+    public void deleteBook(@PathVariable("id") Long id){
         bookService.deleteBook(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
