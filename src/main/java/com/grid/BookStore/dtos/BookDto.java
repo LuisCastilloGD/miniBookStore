150dc24364d3d3e3b314d75bb53b5f19f8744fc0
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-09 15:33:14 -0600

    deleted @Builder

--- src/main/java/com/grid/BookStore/dtos/BookDto.java
+++ src/main/java/com/grid/BookStore/dtos/BookDto.java
@@ -3,13 +3,11 @@ package com.grid.BookStore.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
-@Builder
 public class BookDto {
 
     @NotNull
