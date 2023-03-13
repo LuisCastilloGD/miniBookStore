9f0ea627ab8ef4cb569c3bd17c9ea95f957d1ff9
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-13 11:05:04 -0600

    add Page entity

---
+++ src/main/java/com/grid/BookStore/models/Page.java
@@ -0,0 +1,37 @@
+package com.grid.BookStore.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Table(name = "page", schema="book_store")
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class Page {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NonNull
+    @NotBlank
+    private String content;
+
+    @NonNull
+    @NotBlank
+    private Integer number;
+
+}
