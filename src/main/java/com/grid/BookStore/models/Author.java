72a0e507d8be5bc71200783da7da61ba582fc701
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-13 11:04:52 -0600

    add Author entity

---
+++ src/main/java/com/grid/BookStore/models/Author.java
@@ -0,0 +1,39 @@
+package com.grid.BookStore.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.HashMap;
+
+@Entity
+@Table(name = "author", schema="book_store")
+@Getter
+@Setter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @NotNull
+    @NotBlank
+    private String name;
+    @NonNull
+    @NotNull
+    @NotBlank
+    private String lastname;
+
+}
