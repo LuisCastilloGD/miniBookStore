fe6ef5824433bbeb6a6baabff54400188a1a9334
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-14 14:34:02 -0600

    add PageNotFoundedException

---
+++ src/main/java/com/grid/BookStore/exception/PageNotFoundedException.java
@@ -0,0 +1,8 @@
+package com.grid.BookStore.exception;
+
+public class PageNotFoundedException extends RuntimeException{
+
+    public PageNotFoundedException(Long id) {
+        super(String.format("Page with id %s not found", id));
+    }
+}
