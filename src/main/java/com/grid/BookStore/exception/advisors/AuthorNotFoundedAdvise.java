b0331588b2611ab89863fef8e0b7f17651595d4a
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-14 14:32:53 -0600

    add AuthorNotFounded Advice

---
+++ src/main/java/com/grid/BookStore/exception/advisors/AuthorNotFoundedAdvise.java
@@ -0,0 +1,22 @@
+package com.grid.BookStore.exception.advisors;
+
+import com.grid.BookStore.exception.AuthorNotFoundedException;
+import com.grid.BookStore.exception.BookNotFoundedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class AuthorNotFoundedAdvise {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(AuthorNotFoundedException.class)
+    public String authorNotFoundHandler(final AuthorNotFoundedException e) {
+        return e.getMessage();
+    }
+
+
+}
