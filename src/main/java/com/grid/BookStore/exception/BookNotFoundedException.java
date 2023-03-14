1898954c8db419796b88734a3902fc0c57b42a4f
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-14 14:37:13 -0600

    change to not need id to display exception

--- src/main/java/com/grid/BookStore/exception/BookNotFoundedException.java
+++ src/main/java/com/grid/BookStore/exception/BookNotFoundedException.java
@@ -2,7 +2,7 @@ package com.grid.BookStore.exception;
 
 public class BookNotFoundedException extends RuntimeException{
 
-    public BookNotFoundedException(Long bookId) {
-        super(String.format("Book with id %s not found", bookId));
+    public BookNotFoundedException() {
+        super("Book not found");
     }
 }
