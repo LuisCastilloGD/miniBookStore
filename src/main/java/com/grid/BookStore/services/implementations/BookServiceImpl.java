6f550ab7dcd1d937c7fe19f933354fa660449878
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-14 14:38:33 -0600

    added findByAuthor and finallBooksFromAuthorsPage methods implementations

--- src/main/java/com/grid/BookStore/services/implementations/BookServiceImpl.java
+++ src/main/java/com/grid/BookStore/services/implementations/BookServiceImpl.java
@@ -1,45 +1,50 @@
 package com.grid.BookStore.services.implementations;
 
-import com.grid.BookStore.converters.BookMapper;
-import com.grid.BookStore.models.dtos.BookDto;
 import com.grid.BookStore.exception.BookNotFoundedException;
 import com.grid.BookStore.models.Book;
 import com.grid.BookStore.repositories.BookRepository;
+import com.grid.BookStore.services.AuthorService;
 import com.grid.BookStore.services.BookService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
-    private final BookMapper bookMapper;
-
     @Override
-    public BookDto addBook(final Book book) {
-        return bookMapper.modelToDto(bookRepository.save(book));
+    public Book addBook(final Book book) {
+        if(bookRepository.findByTitle(book.getTitle()).isPresent()){
+            return bookRepository.findByTitle(book.getTitle())
+                                   .orElseThrow(() -> new BookNotFoundedException());
+        }else{
+            return bookRepository.save(book);
+        }
     }
 
     @Override
-    public BookDto getBook(final Long id) {
+    public Book getBook(final Long id) {
         return bookRepository.findById(id)
-                             .map(bookMapper::modelToDto)
-                             .orElseThrow(() -> new BookNotFoundedException(id));
+                             .orElseThrow(() -> new BookNotFoundedException());
     }
 
+    @Override
+    public Book getBookByTitle(final String title) {
+        return bookRepository.findByTitle(title)
+                             .orElseThrow(() -> new BookNotFoundedException());
+    }
 
     @Override
-    public BookDto editBook(final Long id, final Book book) {
+    public Book editBook(final Long id, final Book book) {
         return bookRepository.findById(id)
                             .map(foundedBook -> {
                                 book.setId(foundedBook.getId());
                                 return bookRepository.save(book);
                             })
-                             .map(bookMapper::modelToDto)
-                             .orElseThrow(() -> new BookNotFoundedException(id));
+                             .orElseThrow(() -> new BookNotFoundedException());
     }
 
     @Override
@@ -48,7 +53,18 @@ public class BookServiceImpl implements BookService {
                          .ifPresentOrElse(book -> bookRepository.deleteById(id),
 
                                           () -> {
-                                              throw new BookNotFoundedException(id);
+                                              throw new BookNotFoundedException();
                                             });
     }
+
+    @Override
+    public List<Book> findByAuthor(final String name){
+        return bookRepository.findByAuthors_Name(name).stream().toList();
+    }
+
+    @Override
+    public List<Book> findAllBooksFromAuthorsPage(final Long id){
+        return bookRepository.findAllBooksFromAuthorsPage(id);
+    }
+
 }
