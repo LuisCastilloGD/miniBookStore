002100e8b6c3a7d250764dc175aaa46bf25f1a5a
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-14 14:31:37 -0600

    add AuthorRepository

---
+++ src/main/java/com/grid/BookStore/repositories/AuthorRepository.java
@@ -0,0 +1,33 @@
+package com.grid.BookStore.repositories;
+
+import com.grid.BookStore.models.Author;
+import com.grid.BookStore.models.Book;
+import lombok.NonNull;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+    @Query("select a from Author a where a.name = ?1")
+    Optional<Author> findByName(String name);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM author " +
+           "WHERE id IN(" +
+           "" +
+           "SELECT authors_id FROM " +
+           "book_authors AS ba JOIN  " +
+           "        book_pages AS bp ON bp.book_id=ba.book_id JOIN " +
+           "page ON pages_id = id" +
+           "    WHERE id = ?1" +
+           ")")
+    List<Author> findAuthorsByPageId(Long id);
+
+}
