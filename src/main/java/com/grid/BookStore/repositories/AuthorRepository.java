360c77fbfa1c502213e8bc550eebb4def76d796d
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-14 14:41:17 -0600

    clean imports

--- src/main/java/com/grid/BookStore/repositories/AuthorRepository.java
+++ src/main/java/com/grid/BookStore/repositories/AuthorRepository.java
@@ -1,16 +1,10 @@
 package com.grid.BookStore.repositories;
 
 import com.grid.BookStore.models.Author;
-import com.grid.BookStore.models.Book;
-import lombok.NonNull;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
