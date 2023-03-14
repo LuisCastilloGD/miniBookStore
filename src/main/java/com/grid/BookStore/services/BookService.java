2b559ebaca2cb361caf59214a02c7bbeb55db3d4
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-14 14:38:21 -0600

    added findByAuthor and finallBooksFromAuthorsPage methods

--- src/main/java/com/grid/BookStore/services/BookService.java
+++ src/main/java/com/grid/BookStore/services/BookService.java
@@ -4,13 +4,16 @@ import com.grid.BookStore.models.dtos.BookDto;
 import com.grid.BookStore.models.Book;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 public interface BookService {
 
-    BookDto addBook(Book book);
-    BookDto getBook(Long id);
-    BookDto editBook(Long id, Book book);
+    Book addBook(Book book);
+    Book getBook(Long id);
+    Book getBookByTitle(String title);
+    Book editBook(Long id, Book book);
     void deleteBook(Long id);
-
+    List<Book> findByAuthor(String name);
+    List<Book> findAllBooksFromAuthorsPage(Long id);
 }
